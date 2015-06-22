99a3de94c4263ca6db03fc0b6662323884d2815f
nuno.pinheiro <nuno.pinheiro@qub-it.com>
2015-06-22 18:54:43 +0100

    Bug fixing after test with full dataset

--- src/main/java/org/fenixedu/ulisboa/integration/sas/webservices/ActiveStudentsWebService.java
+++ src/main/java/org/fenixedu/ulisboa/integration/sas/webservices/ActiveStudentsWebService.java
@@ -9,14 +9,14 @@ import java.util.stream.Stream;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-import org.fenixedu.academic.domain.Degree;
-import org.fenixedu.academic.domain.ExecutionSemester;
 import org.fenixedu.academic.domain.ExecutionYear;
-import org.fenixedu.academic.domain.SchoolLevelType;
 import org.fenixedu.academic.domain.student.Registration;
 import org.fenixedu.academic.domain.student.Student;
 import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.bennu.core.util.CoreConfiguration;
+import org.fenixedu.ulisboa.integration.sas.domain.SchoolLevelTypeMapping;
 import org.fenixedu.ulisboa.integration.sas.dto.ActiveStudentBean;
+import org.fenixedu.ulisboa.specifications.domain.idcards.CgdCard;
 import org.joda.time.LocalDate;
 import org.joda.time.YearMonthDay;
 
@@ -46,8 +46,7 @@ public class ActiveStudentsWebService extends BennuWebService {
 
     private boolean isActive(Student student) {
         //TODO review the requirements since the concept of active student for SAS is not the same as in fenix
-//        return !student.getActiveRegistrations().isEmpty();
-        return true;
+        return !student.getActiveRegistrations().isEmpty();
     }
 
     private Stream<Student> calculateStudentsRegisteredInCurrentDay() {
@@ -55,64 +54,59 @@ public class ActiveStudentsWebService extends BennuWebService {
     }
 
     private ActiveStudentBean populateActiveStudent(Student student) {
-        ExecutionSemester actualExecutionSemester = ExecutionSemester.readActualExecutionSemester();
         // TODO review which registration to use
         // Return the first Registration found
 
         ActiveStudentBean activeStudentBean = new ActiveStudentBean();
-//        String name;
         activeStudentBean.setName(student.getName());
         activeStudentBean.setGender(student.getPerson().getGender().toLocalizedString(Locale.getDefault()));
         //information still not available
-        //String mifare;
-        activeStudentBean.setMifare("123456789");
 
-        activeStudentBean.setIdentificationNumber(student.getPerson().getIdentificationDocumentSeriesNumberValue());
+        String mifareCode =
+                student.getPerson().getCgdCardsSet().stream().filter(CgdCard::isValid).map(CgdCard::getMifareCode).findAny()
+                        .orElse("");
+        activeStudentBean.setMifare(mifareCode);
+
+        activeStudentBean.setIdentificationNumber(student.getPerson().getDocumentIdNumber());
         activeStudentBean.setFiscalIdentificationNumber(student.getPerson().getSocialSecurityNumber());
         YearMonthDay dateOfBirthYearMonthDay = student.getPerson().getDateOfBirthYearMonthDay();
         activeStudentBean.setDateOfBirth(dateOfBirthYearMonthDay != null ? dateOfBirthYearMonthDay.toString() : "");
         activeStudentBean.setStudentCode(student.getNumber().toString());
         activeStudentBean.setOriginCountry(student.getPerson().getCountry().getLocalizedName().getContent(Locale.getDefault()));
 
-        /*if (!student.getActiveRegistrations().isEmpty()) {
+        if (!student.getActiveRegistrations().isEmpty()) {
             Registration registration = student.getActiveRegistrations().iterator().next();
-            SchoolLevelType schoolLevelTypeFor = SchoolLevelTypeMapping.getSchoolLevelTypeFor(registration.getDegreeType());
-            if (schoolLevelTypeFor == null) {
-                //Consider all courses without school level as the free course 
+            SchoolLevelTypeMapping schoolLevelTypeMapping = registration.getDegreeType().getSchoolLevelTypeMapping();
+            if (schoolLevelTypeMapping == null) {
+                //Consider all courses without school level type mapping as the free course 
                 activeStudentBean.setDegreeCode(ActiveDegreesWebService.FREE_COURSES_CODE);
             } else {
                 activeStudentBean.setDegreeCode(registration.getDegree().getCode());
                 activeStudentBean.setOficialDegreeCode(registration.getDegree().getMinistryCode());
 
                 ArrayList<ExecutionYear> sortedExecutionYears = getSortedExecutionYears(registration);
-                ExecutionYear currentExecutionYear = sortedExecutionYears.get(sortedExecutionYears.size() - 1);
-                activeStudentBean.setCurrentExecutionYear(currentExecutionYear.toString());
-                activeStudentBean.setEnroledECTTotal(Double.toString(registration.getEnrolmentsEcts(currentExecutionYear)));
+                if (sortedExecutionYears.size() > 0) {
+                    ExecutionYear currentExecutionYear = sortedExecutionYears.get(sortedExecutionYears.size() - 1);
+                    activeStudentBean.setCurrentExecutionYear(currentExecutionYear.getName());
+                    activeStudentBean.setEnroledECTTotal(Double.toString(registration.getEnrolmentsEcts(currentExecutionYear)));
+                    activeStudentBean.setDateOfRegistration(getEnrolmentDate(registration, currentExecutionYear).toString());
+                    activeStudentBean.setRegime(registration.getRegimeType(currentExecutionYear).toString());
+                }
 
                 if (sortedExecutionYears.size() > 1) {
                     ExecutionYear previousExecutionYear = sortedExecutionYears.get(sortedExecutionYears.size() - 1);
-                    activeStudentBean.setCurrentExecutionYear(previousExecutionYear.toString());
-                    activeStudentBean.setEnroledECTTotal(Double.toString(registration.getEnrolmentsEcts(previousExecutionYear)));
+                    activeStudentBean.setPreviousExecutionYear(previousExecutionYear.getName());
+                    activeStudentBean.setEnroledECTTotalInPreviousYear(Double.toString(registration
+                            .getEnrolmentsEcts(previousExecutionYear)));
                     activeStudentBean.setApprovedECTTotalInPreviousYear(getApprovedEcts(registration, previousExecutionYear)
                             .toString());
                 }
 
-                activeStudentBean.setDateOfRegistration(getEnrolmentDate(registration, currentExecutionYear).toString());
                 activeStudentBean.setCurricularYear(Integer.toString(registration.getCurricularYear()));
-                activeStudentBean.setRegime(registration.getRegimeType(currentExecutionYear).toString());
             }
-        } else {*/
-        Degree fakeDegree = Bennu.getInstance().getDegreesSet().iterator().next();
-        activeStudentBean.setDegreeCode(fakeDegree.getCode());
-        activeStudentBean.setOficialDegreeCode(fakeDegree.getMinistryCode());
-        ExecutionYear currentExecutionYear = ExecutionYear.readCurrentExecutionYear();
-        activeStudentBean.setCurrentExecutionYear(currentExecutionYear.toString());
-        activeStudentBean.setEnroledECTTotal("10");
-        activeStudentBean.setPreviousExecutionYear(currentExecutionYear.getPreviousExecutionYear().toString());
-        activeStudentBean.setEnroledECTTotal("10");
-        activeStudentBean.setApprovedECTTotalInPreviousYear("10");
-//        }
-
+        } else {
+            return null;
+        }
         //information will only be available during implementation of academic-treasury
         activeStudentBean.setIsPayingSchool(true);
 
@@ -126,7 +120,7 @@ public class ActiveStudentsWebService extends BennuWebService {
 
     private Double getApprovedEcts(Registration firstRegistration, ExecutionYear previousExecutionYear) {
         return firstRegistration.getEnrolments(previousExecutionYear).stream().filter(e -> e.isApproved())
-                .map(e -> e.getEctsCredits()).reduce((n1, n2) -> n1 + n2).get();
+                .map(e -> e.getEctsCredits()).reduce((n1, n2) -> n1 + n2).orElse(0.0);
     }
 
     private ArrayList<ExecutionYear> getSortedExecutionYears(Registration firstRegistration) {
