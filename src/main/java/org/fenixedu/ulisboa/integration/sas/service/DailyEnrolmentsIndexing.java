db7285641b381d4a6764f28411bc538a833545b7
Paulo Abrantes <paulo.abrantes@qub-it.com>
2015-08-25 10:53:57 +0100

    Bug fix: task must have @Atomic to have a write context Also modified code to handle days with many registrations created

--- src/main/java/org/fenixedu/ulisboa/integration/sas/service/DailyEnrolmentsIndexing.java
+++ src/main/java/org/fenixedu/ulisboa/integration/sas/service/DailyEnrolmentsIndexing.java
@@ -1,17 +1,24 @@
 package org.fenixedu.ulisboa.integration.sas.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.fenixedu.academic.domain.Enrolment;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.signals.DomainObjectEvent;
 import org.fenixedu.bennu.signals.Signal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.FenixFramework;
 import pt.ist.fenixframework.dml.DeletionListener;
 
 //Operations to allow "caching"/"indexing" which enrolments were performed in the current day
 public class DailyEnrolmentsIndexing {
+
+    private static Logger logger = LoggerFactory.getLogger(DailyEnrolmentsIndexing.class);
 
     public static void bindToSignals() {
         Signal.register(Enrolment.SIGNAL_CREATED, associateWithBennu());
@@ -33,6 +40,23 @@ public class DailyEnrolmentsIndexing {
     }
 
     public static void performDailyClean() {
-        Bennu.getInstance().getDailyEnrolmentsSet().forEach(dessociateWithBennu());
+        logger.info("Starting daily index clean for enrolments....");
+        List<Enrolment> dailyEnrolmentsSet = new ArrayList<Enrolment>(Bennu.getInstance().getDailyEnrolmentsSet());
+        int size = dailyEnrolmentsSet.size();
+        int pageSize = 1000;
+        int numberOfPages = size / pageSize + (size % pageSize > 0 ? 1 : 0);
+        for (int i = 0; i < numberOfPages; i++) {
+            logger.debug("Page : " + i + "/" + numberOfPages);
+            int start = i * pageSize;
+            int end = Math.min(start + pageSize, size);
+            performDailyClean(dailyEnrolmentsSet.subList(start, end));
+        }
+        dailyEnrolmentsSet.forEach(dessociateWithBennu());
+        logger.info("Finisheddaily index clean for enrolments....");
+    }
+
+    @Atomic
+    private static void performDailyClean(List<Enrolment> enrolmentsToDissociate) {
+        enrolmentsToDissociate.forEach(dessociateWithBennu());
     }
 }
